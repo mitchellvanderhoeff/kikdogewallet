793c590ab94c903440c8257ee5f60cefd682d53f
Mitchell Vanderhoeff <mitchellvanderhoeff@gmail.com>
2014-03-03 15:48:44 -0500

    I made an oopsie in the gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var nodemon = require('nodemon');
 gulp.task('build', function() {
    bower()
       .pipe(gulp.dest('dist/bower_components/'));
-   gulp.src(['app/scripts/**/*.js', 'app/scripts/*.js'])
+   gulp.src(['app/scripts/**/*.js', 'app/app.js'])
       .pipe(uglify())
       .pipe(gulp.dest('dist/scripts'));
    gulp.src('app/styles/*.css')
