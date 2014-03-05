60e497b7ce765fa33e891f69d7b872673315ba6e
Mitchell Vanderhoeff <mitchellvanderhoeff@gmail.com>
2014-03-04 21:34:44 -0500

    Comment on DB

--- server/db.js
+++ server/db.js
@@ -2,6 +2,12 @@
  * Created by mitch on 2/9/2014.
  */
 
+
+/*
+ * This is where the MongoDB instance is created.
+ * The current setup works for the MongoHQ plugin on Heroku as well as running locally.
+ * If you wish to run on another server, make the appropriate changes here.
+ */
 var MongoJS = require('mongojs');
 
 var db;
